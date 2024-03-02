cf6951a7b3447f25acd18689d990249bb40b7917
Anands-88 <anandsholla8@gmail.com>
2024-03-02 23:13:15 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -15,9 +15,10 @@ function App() {
         <h2 onClick={()=>{setShift("timer")}}
         style={{borderBottom:shift=="timer"?"3px solid white":""}}>
          <GiSandsOfTime className={Style.timer}></GiSandsOfTime> Timer</h2> 
+        <hr size="52" className={Style.LineVertical} />
         <h2 onClick={()=>{setShift("watch")}}
         style={{borderBottom:shift=="watch"?"3px solid white":""}}> 
-        <ImStopwatch className={Style.stopWatch}></ImStopwatch>StopWatch</h2>
+        <ImStopwatch className={Style.stopWatch}></ImStopwatch>Stopwatch</h2>
       </div>
       <div style={{marginTop:-30}}>
         {shift=="timer"?<Timer/>:<StopWatch/> }
